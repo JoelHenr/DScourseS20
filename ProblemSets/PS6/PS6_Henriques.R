bf3953dbb77593276756129821d1e1dae67c2e12
Joel Henriques <joel.c.henriques-1@ou.edu>
2020-03-03 21:22:30 -0600

    Complted PS6

---
+++ ProblemSets/PS6/PS6_Henriques.R
@@ -0,0 +1,33 @@
+library(rvest)
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+library(dplyr)
+library(magrittr) 
+
+df <- read_csv("GDP.csv")
+summary(df)
+
+mean(df$GDP, na.rm = TRUE)
+
+df %>%
+  mutate(GDP = as.factor(GDP)) 
+df %>% 
+  mutate(DATE=as.factor(DATE))
+
+
+df$diff <- df$GDP - lag(df$GDP)
+df$perchnge <- (df$diff/df$GDP)*100
+ggplot(data = df, aes(x=df$DATE,y=perchnge))+ggtitle("Change in US GDP Over Time")+ 
+  xlab("Time") + ylab("US GDP") + geom_line()+theme_clean()
+
+
+
+
+  
+
+  
+
+
+
+
